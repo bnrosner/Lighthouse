318d3f3301642543f81e73f23bd0f3d0f383fe47
Benne Rosner <brosner2016@kellogg.northwestern.edu>
2015-06-01 04:12:46 -0500

    Added nav bar options

--- app/controllers/schools_controller.rb
+++ app/controllers/schools_controller.rb
@@ -1,5 +1,22 @@
 class SchoolsController < ApplicationController
- def new
+ 
+  def index
+    if current_user.admin?
+      @users = School.all
+    else
+      redirect_to root_path, alert: "Nice try buddy"
+    end
+  end
+
+  def show
+    if current_user.admin?
+      @school = School.find_by(id: params["id"])
+    else
+      redirect_to root_path, alert: "Nice try buddy"
+    end
+  end
+
+  def new
     if current_user.admin?
       @school = School.new
     else
@@ -11,7 +28,7 @@ class SchoolsController < ApplicationController
     if current_user.admin?  
       @school = School.create(params["school"])
       if @school.valid?
-        redirect_to courses_url, notice: "Great, you created a school!"
+        redirect_to schools_url, notice: "Great, you created a school!"
       else
         render "new"
       end
@@ -20,11 +37,21 @@ class SchoolsController < ApplicationController
     end
   end
 
+  def edit
+    @school = School.find_by(id: params["id"])
+  end
+
+  def update
+    @school = School.find_by(id: params["id"])
+    @school.update(params["school"])
+    redirect_to schools_url
+  end
+
   def destroy
     if current_user.admin?
       @school = School.find_by(id: params["id"])
       @school.delete
-      redirect_to courses_url
+      redirect_to schools_url
     else
       redirect_to root_path, alert: "Nice try buddy"
     end
