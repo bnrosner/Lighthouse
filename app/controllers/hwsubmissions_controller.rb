ed9f0c25d1713f13c76992718c45d806caa3fd71
Benne Rosner <brosner2016@kellogg.northwestern.edu>
2015-07-22 00:34:29 -0400

    Fixed view for other viewers to see question status

--- app/controllers/hwsubmissions_controller.rb
+++ app/controllers/hwsubmissions_controller.rb
@@ -1,12 +1,14 @@
 class HwsubmissionsController < ApplicationController
 
  def create
-     @hwsubmission = HWSubmission.new
      @homework = Homework.find_by(id: params["format"])
-     @hwsubmission.homework_id = @homework.id
-     @student = Student.find_by(course_id: @homework.course_id, user_id: current_user.id)
-     @hwsubmission.student_id = @student.id 
-     @hwsubmission.save
+     @homework.num_questions.times do
+       @hwsubmission = HWSubmission.new
+       @hwsubmission.homework_id = @homework.id
+       @student = Student.find_by(course_id: @homework.course_id, user_id: current_user.id)
+       @hwsubmission.student_id = @student.id 
+       @hwsubmission.save
+     end
      redirect_to root_path
    end
 end
